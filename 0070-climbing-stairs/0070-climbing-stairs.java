245f040087d861a6be6f6a4a6b7c89e4f46a9643
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2024-10-17 17:01:33 +0530

    Time: 0 ms (100%), Space: 40 MB (91.2%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n==1){
+            return 1;
+
+        }
+        int curr=0,prev1=1,prev2=1;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+        
+        
+    }
+}
