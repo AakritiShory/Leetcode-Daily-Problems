31cbd465c64bd415fc60e9d39c7d5404ae0e3d6c
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2024-12-04 11:32:03 +0530

    Time: 0 ms (100%), Space: 40.2 MB (56.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,20 @@
 class Solution {
+    static int[] dp;
+    public int fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=0){  //agr pehle se voh no stored
+            return dp[n]; //return krdo uski value
+        }
+        int ans=fibo(n-1)+fibo(n-2);
+            dp[n]=ans;
+            return ans;
+    }
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        int ans=fib(n-1)+fib(n-2);
-        return ans;
+        dp=new int[n+1];//index from 0 to n
+        return fibo(n);
+        
         
     }
 }
