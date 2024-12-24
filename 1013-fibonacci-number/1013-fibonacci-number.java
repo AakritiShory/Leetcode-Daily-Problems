f4e3c28d28bb53cbca699182b2f9c06251b0714b
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2024-12-24 21:43:45 +0530

    Time: 0 ms (100%), Space: 40.4 MB (51.87%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,18 @@
 class Solution {
-    public int fib(int n) {
+    public int fibo(int n,int[] dp){
         if(n<=1){
             return n;
         }
-        int dp[]=new int[n+1];
-        dp[0]=0; dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(dp[n]!=0){
+            return dp[n];
         }
+        int ans=fibo(n-1,dp)+fibo(n-2,dp);
+        dp[n]=ans;
         return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        return fibo(n,dp);
+        
     }
 }
