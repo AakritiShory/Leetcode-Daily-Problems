e4757caa49c644a0374339ef98e48ceecce34523
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2024-12-24 21:36:18 +0530

    Time: 0 ms (100%), Space: 40.5 MB (29.44%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -3,8 +3,8 @@ class Solution {
         if(n<=1){
             return n;
         }
-        int[] dp=new int[n+1];
-        dp[0]=0;dp[1]=1;
+        int dp[]=new int[n+1];
+        dp[0]=0; dp[1]=1;
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
